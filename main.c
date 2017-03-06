ffde5dfa6435fbf090030ae2e91f04d271ee6455
Benjamin <ben639dx@gmail.com>
2017-03-06 19:01:23 +0100

    can read, mayb get stat

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+
+/*
+--	Checks if there are a hidden file
+*/
 
 int		check_dot(char *str)
 {
@@ -13,6 +18,10 @@ int		check_dot(char *str)
 	else
 		return (0);
 }
+
+/*
+**	Reads in a dir but avoid hidden files
+*/
 
 int		read_in_dir(char *dir)
 {
@@ -33,16 +42,33 @@ int		read_in_dir(char *dir)
 		while ((file = readdir(rep)) != NULL)
 		{
 			if (check_dot(file->d_name) != 1)
-				printf("%s\n", file->d_name);
+				ft_putstr(file->d_name);
 		}
 		closedir(rep);	
 	}
 	return (1);
+}
 
+/*
+**	Give stats of a file
+*/
+
+int		stats_file(char *file)
+{
+	struct stat fileStat;
+
+	if (file == NULL)
+		printf("no file");
+	if (stat(file, fileStat) < 0)
+		return (0);
+	ft_putstr("Taille du fichier : ");
+	ft_putnbr(fileStat.st_size);
 }
 
 int main (int argc, char **argv)
 {
 	read_in_dir(argv[1]);
+
+	stats_file(argv[1]);
 	return (1);
 }
